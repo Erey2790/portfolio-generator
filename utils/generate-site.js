04e03e84fc0f7f569226019565b7c4da97eac5b6
ElmerPC <erey2790@gmail.com>
2022-01-20 13:24:19 -0600

    final generate-sit.js

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -18,3 +18,19 @@ const writeFile = fileContent => {
         });
     });
 };
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('.src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Stylesheet Created!'
+            });
+        });
+    });
+};
+module.exports = { writeFile, copyFile };
